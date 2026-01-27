5e14d253067d4067eb78afb625d1647ec0766f4d
VishalGhuge111 <vishalghugesit@gmail.com>
2026-01-28 03:18:34 +0530

    switch email provider to brevo smtp

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,29 @@
 import nodemailer from "nodemailer";
 
-const sendEmail = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    text,
-  });
+const sendEmail = async (to, subject, text) => {
+  try {
+    const info = await transporter.sendMail({
+      from: `"CampusFlow" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      text
+    });
+
+    console.log("EMAIL SENT:", info.messageId);
+  } catch (err) {
+    console.error("EMAIL ERROR:", err);
+    throw err;
+  }
 };
 
 export default sendEmail;
